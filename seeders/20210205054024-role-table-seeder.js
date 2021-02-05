c380671d654f445da83f59d6e788808ddb15519c
Sushant <sushantjadhav2010@gmail.com>
2021-02-05 11:20:24 +0530

    Role Seeder Aded

--- seeders/20210205054024-role-table-seeder.js
+++ seeders/20210205054024-role-table-seeder.js
@@ -1,7 +1,9 @@
 'use strict';
+let moment = require('moment');
+let now = moment();
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -12,6 +14,27 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    await queryInterface.bulkDelete("roles", null, {
+      truncate: true,
+      cascade: true,
+    });
+
+    return queryInterface.bulkInsert('roles', [
+      {
+        id: 1,
+        role_name: 'Super Admin',
+        is_active: 1,
+        created_at: now.format('YYYY-MM-DD hh:mm:ss'),
+        updated_at: now.format('YYYY-MM-DD hh:mm:ss'),
+      },
+      {
+        id: 2,
+        role_name: 'Customer',
+        is_active: 1,
+        created_at: now.format('YYYY-MM-DD hh:mm:ss'),
+        updated_at: now.format('YYYY-MM-DD hh:mm:ss'),
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
